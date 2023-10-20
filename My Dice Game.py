b52fba455c88c4f8ec04f80d57e78471d8ebba86
jacob <jacobodiko2@gmail.com>
2023-10-20 15:41:28 +0300

    My number1 commit

---
+++ My Dice Game.py
@@ -0,0 +1,4 @@
+import random
+choices=range(1,7)
+computer=random.choice(choices) #The computer picks a random number (1,2,3,4,5,6)
+print("The Dice's face is: "+str(computer)) #The number is converter into string and displayed with the other strings
